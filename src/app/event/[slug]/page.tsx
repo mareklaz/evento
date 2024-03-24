56533b41d8c438cdaf544556ee5d52e4e1641c54
marek laz <mareklaz4@gmail.com>
2024-03-24 23:43:31 +0100

    creacion loading utils

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import H1 from '@/components/h1';
+import { sleep } from '@/lib/utils';
 import Image from 'next/image';
+import React from 'react';
 
 type EventPageProps = {
 	params: {
@@ -9,13 +11,12 @@ type EventPageProps = {
 
 export default async function EventPage({ params }: EventPageProps) {
 	const slug = params.slug;
-	const response = await fetch(
-		`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-	);
+	await sleep(2000);
+	const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`);
 	const event = await response.json();
 	return (
 		<main>
-			<section className='relative h-[360px] overflow-hidden'>
+			<section className='relative overflow-hidden flex justify-center items-center py-14 md:py-20'>
 				<Image
 					src={event.imageUrl}
 					className='object-cover blur-3xl z-0'
@@ -25,24 +26,52 @@ export default async function EventPage({ params }: EventPageProps) {
 					priority
 					sizes='(max-width: 1280px) 100vw, 1280px'
 				/>
-				<div>
+				<div className='z-1 relative flex flex-col lg:flex-row gap-6 lg:gap-16 justify-center'>
 					<Image
 						src={event.imageUrl}
 						alt={event.name}
 						width={300}
 						height={201}
-						className='z-1 relative'
+						className='rounded-xl border-2 border-white/50 object-cover'
 					/>
-				</div>
-				<div>
-					{/* Display date in day name comma name of the mount and then day number of the mount  */}
-					<p>{new Date(event.date).toDateString()}</p>
-					<H1 className=''>{event.name}</H1>
-					<p>
-						Organized by <span className='italic'>{event.organizerName}</span>
-					</p>
+					<div className='flex flex-col'>
+						<p className=' text-white/75'>
+							{new Date(event.date).toLocaleDateString('en-US', {
+								weekday: 'long',
+								month: 'long',
+								day: 'numeric',
+							})}
+						</p>
+						<H1 className='mb-2 mt-1 whitespace-nowrap lg:text-5xl '>{event.name}</H1>
+						<p className='whitespace-nowrap text-xl text-white/75'>
+							Organized by <span className='italic'>{event.organizerName}</span>
+						</p>
+						<button className='bg-white/20 lg:mt-auto mt-5 text-lg capitalize w-[95vw] sm:w-full py-2 rounded-md border-white/10 border-2 bg-blur state-effects'>
+							Get tickets
+						</button>
+					</div>
 				</div>
 			</section>
+			<div className='min-h-[75vh] text-center px-5 py-16'>
+				<Section>
+					<SectionHeading>About this event</SectionHeading>
+					<SectionContent>{event.description}</SectionContent>
+				</Section>
+				<Section>
+					<SectionHeading>Location</SectionHeading>
+					<SectionContent>{event.location}</SectionContent>
+				</Section>
+			</div>
 		</main>
 	);
+}
+//
+function Section({ children }: { children: React.ReactNode }) {
+	return <section className='mb-12'>{children}</section>;
+}
+function SectionHeading({ children }: { children: React.ReactNode }) {
+	return <h2 className='text-2xl mb-8'>{children}</h2>;
+}
+function SectionContent({ children }: { children: React.ReactNode }) {
+	return <p className='max-w-4xl mx-auto text-lg leading-8 text-white/75'>{children}</p>;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...classes: ClassValue[]) {
 	return twMerge(clsx(classes));
 }
+
+export async function sleep(ms: number = 1000) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
